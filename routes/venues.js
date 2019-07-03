a88e553b0a98c014ce3879d2ab77d8de4f971d97
samay-v <samay12231998@gmail.com>
2019-07-03 16:31:02 +0530

    add new, create, show, edit, update and destroy routes

--- routes/venues.js
+++ routes/venues.js
@@ -14,11 +14,62 @@ router.get("/venues", (req, res)=>{
     })
 })
 
-//show individual venue
-// router.get("/venues/:id",(req, res)=>{
-//     Venue.findById(req.params.id,(err, foundVenue)=>{
-//         if err
-//     })
-// })
+//form to add a new venue
+router.get("/venues/new", (req, res)=>{
+    res.render("venue/new");
+});
+
+//Create new venue
+router.post("/venues",(req, res)=>{
+    Venue.create(req.body.venue, (err, newVenue)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/venues/"+newVenue._id);
+        }
+    });
+});
+
+// show individual venue
+router.get("/venues/:id",(req, res)=>{
+    Venue.findById(req.params.id,(err, foundVenue)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("venue/show", {venue: foundVenue});
+        }
+    });
+});
+
+// form to edit a venue 
+router.get("/venues/:id/edit", (req, res)=>{
+    Venue.findById(req.params.id,(err, foundVenue)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render("venue/edit", {venue: foundVenue});
+        }
+    });
+});
+
+//Update route
+router.put("/venues/:id", (req, res)=>{
+    Venue.findByIdAndUpdate(req.params.id, req.body.venue, (err, updatedVenue)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/venues/" + updatedVenue._id);
+        }
+    });
+});
+
+//Destroy route
+router.delete("/venues/:id",(req, res)=>{
+    Venue.findByIdAndRemove(req.params.id, (err)=>{
+        if(err){
+            console.log(err);
+        }
+    });
+});
 
 module.exports = router;
