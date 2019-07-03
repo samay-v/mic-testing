d6439182787e4afff3223d9c5713ac7ba073214b
samay-v <samay12231998@gmail.com>
2019-07-03 11:54:31 +0530

    add venues routes

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ const express = require("express"),
     mongoose = require("mongoose"),
     Venue = require("./models/venue"),
     seed = require("./seed"),
-    indexRoutes = require("./routes/index.js");
+    indexRoutes = require("./routes/index.js"),
+    venueRoutes = require("./routes/venues");
 
 //configure express
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 //configure mongoose
-mongoose.connect("mongodb://localhost:27017/mic_testing", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/mic_testing", { useNewUrlParser: true });
 // seed();
 
 //routes
 app.use(indexRoutes);
+app.use(venueRoutes);
 
 //start server
 const port = 3000;
 app.listen(port, () => {
-    console.log("server is working for MicTesting on port "+port);
+    console.log("server is working for MicTesting on port " + port);
 });
