3d8893fc2916a0cb7a160e1bcf0b35cf7d123c76
samay-v <samay12231998@gmail.com>
2019-07-05 10:44:18 +0530

    post route to post a review

---
+++ routes/reviews.js
@@ -0,0 +1,37 @@
+const express = require("express"),
+    router = express.Router(),
+    Venue = require("../models/venue"),
+    Review = require("../models/review");
+
+//post forms to mongoDb venues collection
+router.post("/venues/:id/review", (req, res) => {
+    Venue.findById(req.params.id).populate("reviews").exec((err, venue)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            Review.create(req.body.review, (err, review) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    review.save();
+                    venue.reviews.push(review);
+                    let sumRating=0;
+                    venue.reviews.forEach(review => {
+                        sumRating += review.rating
+                    });
+                    venue.rating=Math.round(sumRating/venue.reviews.length);
+                    venue.save((err, venue)=>{
+                        if(err){
+                            console.log(err);
+                        }else{
+                            res.redirect("/venues/" + venue._id);
+                        }
+                        }
+                    );
+                }
+            });
+        }
+    });
+});
+
+module.exports = router;
