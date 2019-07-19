c8feb3f603148f2b6272e3bcd8077329af630121
samay-v <samay1231998@gmail.com>
2019-07-19 15:34:02 +0530

    modify rating stars to become readonly where needed

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,5 +1,8 @@
 // rating logic
+
 $('.ui.rating').rating();
+
+$('.ui.rating.index').rating('disable');
 
 $('#newRating').rating('setting', 'onRate', function (value) {
     $("#ratingInput").val($('#newRating').rating("get rating"))
